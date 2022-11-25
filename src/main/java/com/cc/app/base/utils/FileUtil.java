aa386c2b59edc221f3e4bb22d705dd3d74a8d4c0
chenjbd <chenjbdc@163.com>
2022-11-26 00:58:00 +0800

    装卸工地费用一览表excel导出功能

--- src/main/java/com/cc/app/base/utils/FileUtil.java
+++ src/main/java/com/cc/app/base/utils/FileUtil.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.InputStream;
 
 public class FileUtil {
     private static Logger logger = LoggerFactory.getLogger(FileUtil.class);
@@ -53,6 +54,30 @@ public class FileUtil {
                 path.delete();
             }
         }
+    }
+
+    public static InputStream getResourcesFileInputStream(String fileName) {
+        return Thread.currentThread().getContextClassLoader().getResourceAsStream("" + fileName);
+    }
+
+    public static String getPath() {
+        return FileUtil.class.getResource("/").getPath();
+    }
+
+    public static File createNewFile(String pathName) {
+        File file = new File(getPath() + pathName);
+        if (file.exists()) {
+            file.delete();
+        } else {
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+        }
+        return file;
+    }
+
+    public static File readFile(String pathName) {
+        return new File(getPath() + pathName);
     }
 
 }
--- src/main/java/com/cc/app/core/controller/CxtjController.java
+++ src/main/java/com/cc/app/core/controller/CxtjController.java
@@ -1,10 +1,15 @@
 package com.cc.app.core.controller;
 
 import cn.hutool.core.util.StrUtil;
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.fill.FillConfig;
 import com.cc.app.base.common.CurrentUser;
 import com.cc.app.base.common.PageModel;
 import com.cc.app.base.common.RtnData;
 import com.cc.app.base.model.LoginUser;
+import com.cc.app.base.utils.FileUtil;
 import com.cc.app.core.dao.CxtjMapper;
 import com.cc.app.core.model.CreateBox;
 import com.cc.app.core.service.CreateBoxService;
@@ -19,8 +24,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.net.URLEncoder;
+import java.util.*;
 
 @Api(value = "/core/api/cxtj")
 @RestController
@@ -86,6 +97,39 @@ public class CxtjController {
         return RtnData.ok(result);
     }
 
+    @ApiOperation(value = "装卸工装货统计Excel",notes = "装卸工装货统计Excel")
+    @PostMapping(value = "/zxgzhtj-excel")
+    public void zxgzhtjExcel(
+            HttpServletResponse response,
+            @CurrentUser LoginUser user,
+            @RequestBody Map<String,Object> params) {
+        params.put("corpNo",user.getUnitNo());
+        Page page = mapper.zxgzhtj(params);
+        try{
+            String startDate = (String) params.get("startDate");
+            String endDate = (String) params.get("endDate");
+            startDate = startDate == null ? "" : startDate;
+            endDate = endDate == null ? "" : endDate;
+            String tjqj = startDate + " ～ " + endDate;//统计区间
+            String gdmc = (String) params.get("stevedoreName");//工队名称
+            params.put("tjqj", tjqj);
+            params.put("gdmc", gdmc);
+            //统计结果
+            DoubleSummaryStatistics cubicNumDoubleSummaryStatistics = page.stream().mapToDouble(item->
+                    ((BigDecimal)((Map)item).get("cubicNum")).doubleValue()).summaryStatistics();
+            DoubleSummaryStatistics supervisedCbmDoubleSummaryStatistics = page.stream().mapToDouble(item->
+                    ((BigDecimal)((Map)item).get("supervisedCbm")).doubleValue()).summaryStatistics();
+            IntSummaryStatistics constIntSummaryStatistics = page.stream().mapToInt(item->
+                    ((BigDecimal) ((Map)item).get("supervisedCbm")).intValue()).summaryStatistics();
+            params.put("cubicNumSum", cubicNumDoubleSummaryStatistics.getSum());
+            params.put("supervisedCbmSum", supervisedCbmDoubleSummaryStatistics.getSum());
+            params.put("costSum", constIntSummaryStatistics.getSum());
+            this.exportInfo(response, "zxgzhtj", page, params);
+        }catch (Exception e){
+            logger.error(e.getMessage(), e);
+        }
+    }
+
     @ApiOperation(value = "进仓公司统计",notes = "进仓公司统计")
     @GetMapping(value = "/jcgstj")
     public Object jcgstj(@RequestParam(required = false, defaultValue = "20")int pageSize,
@@ -113,4 +157,41 @@ public class CxtjController {
         PageModel result =  new PageModel(pageInfo);
         return RtnData.ok(result);
     }
+
+    /**
+     * 导出excel通用方法
+     * @param response 响应流
+     * @param templateName 模板名
+     * @param list 列表数据
+     * @param params 参数数据
+     * @throws IOException
+     */
+    public void exportInfo(HttpServletResponse response, String templateName,
+                           List<Map> list, Map<String,Object> params) throws IOException {
+        OutputStream out = null;
+        BufferedOutputStream bos = null;
+        try {
+            String templateFileName = FileUtil.getPath() + "templates" + File.separator + templateName + ".xls";
+            response.setContentType("application/vnd.ms-excel");
+            response.setCharacterEncoding("utf-8");
+            String fileName = URLEncoder.encode( templateName + ".xls", "utf-8");
+            response.setHeader("Content-disposition", "attachment; filename=" + new String(fileName.getBytes("UTF-8"), "ISO-8859-1"));
+
+            out = response.getOutputStream();
+            bos = new BufferedOutputStream(out);
+
+            //读取Excel
+            ExcelWriter excelWriter = EasyExcel.write(bos).withTemplate(templateFileName).build();
+            WriteSheet writeSheet = EasyExcel.writerSheet().build();
+
+            //list params 是查询并需导出的数据，并且里面的字段和excel需要导出的字段对应
+            FillConfig fillConfig = FillConfig.builder().forceNewRow(Boolean.TRUE).build();
+            excelWriter.fill(list, fillConfig, writeSheet);
+            excelWriter.fill(params, writeSheet);
+            excelWriter.finish();
+            bos.flush();
+        } catch (Exception e) {
+        }
+    }
+
 }
