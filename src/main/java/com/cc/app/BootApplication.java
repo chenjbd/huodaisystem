ef239c78f2868c2050beb4046f0f68c3aed7aef8
chenjbd <chenjbdc@163.com>
2022-11-30 14:20:27 +0800

    配置war打包

--- src/main/java/com/cc/app/BootApplication.java
+++ src/main/java/com/cc/app/BootApplication.java
@@ -3,16 +3,20 @@ package com.cc.app;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(
-        scanBasePackages = {"com.cc.app","mapping"},
-        exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-        })//禁用activiti登录验证
+        scanBasePackages = {"com.cc.app","mapping"})
 @MapperScan({"com.cc.app.*.dao"})
 @EnableAsync
-public class BootApplication {
+public class BootApplication extends SpringBootServletInitializer {//支持war包
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BootApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
     }
